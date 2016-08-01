e3bd6a44e4d282eb8b8b265f9ad571d45874d03f
alcwu7 <alcwu7@yahoo.com>
2016-08-01 17:23:59 -0400

    Add files via upload

--- projectweek/main1.py
+++ projectweek/main1.py
@@ -2,32 +2,27 @@ from google.appengine.api import users
 import jinja2
 import os
 import webapp2
+import models
 from google.appengine.ext import ndb
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
-    
-class User(ndb.Model):
-  first_name = ndb.StringProperty()
-  last_name = ndb.StringProperty()
 
 class LoginHandler(webapp2.RequestHandler):
   def get(self):
     user = users.get_current_user()
     if user:
       email_address = user.nickname()
-      user_input = User.get_by_id(user.user_id())
+      user_input = models.User.get_by_id(user.user_id())
       signout_link_html = '<a href="%s">sign out</a>' % (
           users.create_logout_url('/'))
       if user_input:
-        home_html = jinja_environment.get_template('Templates/home.html')
         self.response.write('''
             Welcome %s %s (%s)! <br> %s <br>''' % (
               user_input.first_name,
               user_input.last_name,
               email_address,
               signout_link_html))
-        self.response.write(home_html.render())
       else:
         self.response.write('''
             Welcome to our site, %s!  Please sign up! <br>
@@ -44,7 +39,6 @@ class LoginHandler(webapp2.RequestHandler):
           users.create_login_url('/')))
 
   def post(self):
-    home_html = jinja_environment.get_template('Templates/home.html')
     user = users.get_current_user()
     if not user:
       self.error(500)
@@ -56,8 +50,29 @@ class LoginHandler(webapp2.RequestHandler):
     user_input.put()
     self.response.write('Thanks for signing up, %s!' %
         user_input.first_name)
-    self.response.write(home_html.render())
+
+class AccompHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/accomp.html')
+        self.response.write(template.render())
+
+
+    def post(self):
+        template = jinja_environment.get_template('templates/thank_you.html')
+        accomp_info = {
+            'feeling_answer': self.request.get('feeling'),
+            'accomp_info_answer': self.request.get('accomp_text')
+        }
+        accomp_info_record = models.Accomplishments(
+            feeling = accomp_info['feeling_answer'],
+            accomp_info = accomp_info['accomp_info_answer'],
+        )
+        accomp_info_record.put()
+        self.response.write(template.render())
+
+
 
 app = webapp2.WSGIApplication([
-  ('/', LoginHandler)
+  ('/', LoginHandler),
+  ('/accomp', AccompHandler)
 ], debug=True)
